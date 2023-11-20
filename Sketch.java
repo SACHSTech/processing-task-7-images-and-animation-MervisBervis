6fb375fd2614efba78a1b261ae1b43aceea40c98
MervisBervis <bailin.cheng25@ycdsbk12.ca>
2023-11-19 22:41:51 -0500

    Bunny

--- Sketch.java
+++ Sketch.java
@@ -1,14 +1,34 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
-	
+  // Variables
+  PImage imgBackground;
+	PImage imgBunny;
+  
+  float fltBunnyX = -100;
+  float fltBunnyY = 0;
+
+  float fltBunnySpeedX = 2;
+  float fltBunnySpeedY = 1;
+
+  float fltCirX = 100;
+  float fltCirY = width/10;
+
+  float fltCirSpeedX = 2;
+  float fltCirSpeedY = 1;
+
+  float cosAngle = 0;
+  float sinAngle = 0;
+  float fltBunnyAngle = 0;
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
-	// put your size call here
-    size(400, 400);
+
+	// Size call
+    size(1000, 600);
   }
 
   /** 
@@ -16,21 +36,64 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
+    
+    // Load the images into the program
+    imgBunny = loadImage("bunny.png"); 
+    
+    imgBunny.resize(imgBunny.width/4,imgBunny.height/4);
     background(210, 255, 173);
+    
+  imgBackground = loadImage("garden.jpg");
+
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  // Generate the background
+  background(128);
+  image(imgBackground, 0, 0);
+{
+  // Generate the bunny
+   image(imgBunny, fltBunnyX, fltBunnyY);
+    fltBunnyX += 1;
+
+    // Make the bunny move in a wave motion
+    fltBunnyX = (fltBunnyAngle) * 120; 
+    fltBunnyY = 250 + cos(fltBunnyAngle) * 80;
+
+    fltBunnyAngle += 0.04;
+
+    // Reset the bunny
+    if (fltBunnyAngle > 8) {
+      fltBunnyAngle = -1;
+
   }
-  
-  // define other methods down here.
+
+  // Generate the oval
+  ellipse(fltCirX, fltCirY, width / 5, height / 5);
+
+  // Move the oval
+  fltCirX += fltCirSpeedX;
+  fltCirY += fltCirSpeedY;
+
+  // Edge detection for the oval
+  if (fltCirX < 10 || fltCirX > width - 100) {
+    fltCirSpeedX *= -1;
+  }
+
+  if (fltCirY < 10 || fltCirY > height - 80) {
+    fltCirSpeedY *= -1;
+{
+        }
+      }
+    }
+  }
 }
+
+
+
+
+
